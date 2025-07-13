907491f5f8043bbc05594d35b17ab0d36ba7ed2b
jacko103 <z5358585@ad.unsw.edu.au>
2025-07-13 11:15:53 +1000

    Added turning file

---
+++ Turning.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include "math.h"
+
+namespace mtrn3100 {
+
+
+class Turning {
+public:
+    Turning() : {
+
+    }
+
+    void turn() {
+
+    }
+
+private:
+
+};
+
+}  // namespace mtrn3100
